9cd774cc5feac2bbe74d64168320c848b24b622f
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-05-14 12:24:59 +0900

    三角形描画

--- DirectXGame/Shader.cpp
+++ DirectXGame/Shader.cpp
@@ -2,12 +2,9 @@
 #include <d3dcompiler.h>
 #include <dxcapi.h>
 #include "Shader.h"
-#include "MiscUtility.h"
 #pragma comment(lib, "dxcompiler.lib")
 
-void Shader::Load(const std::wstring& filePath, const std::wstring& shaderModel) {
-
-	std::string mbShaderModel = 
+void Shader::Load(const std::wstring& filePath, const std::string& shaderModel) {
 
 	ID3DBlob* shaderBlob = nullptr;
 	ID3DBlob* errorBlob = nullptr;
@@ -32,89 +29,7 @@ void Shader::Load(const std::wstring& filePath, const std::wstring& shaderModel)
 	blob_ = shaderBlob;
 }
 
-//シェーダーファイルを読み込み、コンパイルする
-//外部コンパイラ版、シェーダーモデル6.0以上で利用する
-void Shader::LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel) {
-	static IDxcUtils* dxcUtils = nullptr;
-	static IDxcCompiler3* dxcCompiler = nullptr;
-	static IDxcIncludeHandler* includeHandler = nullptr;
-
-	HRESULT hr;
-
-	if (dxcUtils == nullptr) {
-		hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
-		assert(SUCCEEDED(hr));//うまくいかなかったときは起動できない
-	}
-	if (dxcCompiler == nullptr) {
-		hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
-		assert(SUCCEEDED(hr));//うまくいかなかったときは起動できない
-	}
-	if (includeHandler == nullptr) {
-		hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
-		assert(SUCCEEDED(hr));//うまくいかなかったときは起動できない
-	}
-	//1．hlslファイルを読む
-	IDxcBlobEncoding* shaderSource = nullptr;
-	hr = dxcUtils->LoadFile(filePath.c_str(), nullptr, &shaderSource);
-	assert(SUCCEEDED(hr));
-
-	//読み込んだファイルの内容をDxcBufferに設定する
-	DxcBuffer shaderSourceBuffer{};
-	shaderSourceBuffer.Ptr = shaderSource->GetBufferPointer();
-	shaderSourceBuffer.Size = shaderSource->GetBufferSize();
-	shaderSourceBuffer.Encoding = DXC_CP_UTF8;
-
-	//2.Compileする
-	//Compileに必要なコンパイルオプションの準備
-	LPCWSTR arguments[] = {
-	    filePath.c_str(), // コンパイル対象のhlslファイル名
-	    L"-E",
-	    L"main", // エントリーポイントの指定。
-	    L"-T",
-	    shaderModel.c_str(), // ShaderProfileの設定
-	    L"-Zi",
-	    L"- Qembed_debug", // デバッグ用の情報を埋め込む
-	    L"-Od",            // 最適化を外しておく
-	    L"-Zpr",           // メモリレイアウトは行優先
-	};
-
-	//実際にShaderをコンパイルする
-	IDxcResult* shaderResult = nullptr;
-	hr = dxcCompiler->Compile(
-		&shaderSourceBuffer,       //読み込んだファイル
-		arguments,                 //コンパイルオプション
-		_countof(arguments),       //コンパイルオプションの数
-		includeHandler,            //includeが含まれた諸々
-		IID_PPV_ARGS(&shaderResult)//コンパイル結果
-	);
-	//コンパイルエラーではなくdxcが起動できないなど致命的な状況
-	assert(SUCCEEDED(hr));
-
-	//3.警告・エラーがでていないか確認する
-	IDxcBlobUtf8* shaderError = nullptr;
-	IDxcBlobWide* nameBlob = nullptr;
-	shaderResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&shaderError), &nameBlob);
-	if (shaderError != nullptr && shaderError->GetStringLength() != 0) {
-		OutputDebugStringA(shaderError->GetStringPointer());
-		assert(false);
-	}
-
-	//4.Compile結果を受け取る
-	IDxcBlob* shaderBlob = nullptr;
-	hr = shaderResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&shaderBlob), &nameBlob);
-	assert(SUCCEEDED(hr));
-
-	//もう使わないリソースを解放
-	shaderSource->Release();
-	shaderResult->Release();
-
-	//実行用のバイナリを取っておく
-	dxcBlob_ = shaderBlob;
-}
-
 ID3DBlob* Shader::GetBlob() { return blob_; }
-
-IDxcBlob* Shader::GetDxcBlob() { return dxcBlob_; }
 
 // コンストラクタ
 Shader::Shader() {}
@@ -124,9 +39,5 @@ Shader::~Shader() {
 	if (blob_ != nullptr) {
 		blob_->Release();
 		blob_ = nullptr;
-	}
-	if (dxcBlob_ != nullptr) {
-		dxcBlob_->Release();
-		dxcBlob_ = nullptr;
 	}
 }
--- DirectXGame/Shader.h
+++ DirectXGame/Shader.h
@@ -1,19 +1,15 @@
 #pragma once
 
 #include <d3d12.h>
-#include <d3dcompiler.h>
-#include <dxcapi.h>
 #include <string>
 
 class Shader {
 public:
 	// シェーダーファイルを読み込み、コンパイル済みのデータを生成する
-	void Load(const std::wstring& filePath, const std::wstring& shaderModel);
-	void LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel);
+	void Load(const std::wstring& filePath, const std::string& shaderModel);
 
 	// 生成したコンパイル済みデータを取得する
 	ID3DBlob* GetBlob();
-	IDxcBlob* GetDxcBlob();
 
 	// コンストラクト
 	Shader();
@@ -22,5 +18,4 @@ public:
 
 private:
 	ID3DBlob* blob_ = nullptr; // コンストラクトで初期化しなくていい
-	IDxcBlob* dxcBlob_ = nullptr;
 };
