9cf8a03f0a825e391c1e57bc5de734d899019aee
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-05-29 16:47:15 +0900

    エラー

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,6 +1,7 @@
 #include "KamataEngine.h"
 #include "RootSignature.h"
 #include "PipelineState.h"
+#include "VertexBuffer.h"
 #include "Shader.h"
 #include <Windows.h>
 // #include <d3dcompiler.h>
@@ -18,7 +19,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// DirectXCommonのインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-
 	// DirectXCommonクラスが管理している、ウィンドウの幅と高さの値の取得
 	int32_t w = dxCommon->GetBackBufferWidth();
 	int32_t h = dxCommon->GetBackBufferHeight();
@@ -89,42 +89,46 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	PipelineState pipelineState;
 	SetupPipelineState(pipelineState, rs, vs, ps);
 
-	// VertexResourceの生成----------------------------------------------------------------
-	// 頂点リソース用のヒープの設定
-	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; // CPUから書き込むヒープ
-	// 頂点リソースの設定
-	D3D12_RESOURCE_DESC vertexResourceDesc{};
-	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER; // バッファ
-	vertexResourceDesc.Width = sizeof(Vector4) * 3;                 // リソースのサイズ。今回はVector4を3頂点分
-	vertexResourceDesc.Height = 1;
-	vertexResourceDesc.DepthOrArraySize = 1;
-	vertexResourceDesc.MipLevels = 1;
-	vertexResourceDesc.SampleDesc.Count = 1;
-	// バッファの場合はこれにする決まり
-	vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-	// 実際に頂点リソースを生成する
-	ID3D12Resource* vertexResource = nullptr;
-	HRESULT hr = dxCommon->GetDevice()->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
-	assert(SUCCEEDED(hr)); // うまくいかなかったときは起動できない
+	//// VertexResourceの生成----------------------------------------------------------------
+	//// 頂点リソース用のヒープの設定
+	//D3D12_HEAP_PROPERTIES uploadHeapProperties{};
+	//uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; // CPUから書き込むヒープ
+	//// 頂点リソースの設定
+	//D3D12_RESOURCE_DESC vertexResourceDesc{};
+	//vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER; // バッファ
+	//vertexResourceDesc.Width = sizeof(Vector4) * 3;                 // リソースのサイズ。今回はVector4を3頂点分
+	//vertexResourceDesc.Height = 1;
+	//vertexResourceDesc.DepthOrArraySize = 1;
+	//vertexResourceDesc.MipLevels = 1;
+	//vertexResourceDesc.SampleDesc.Count = 1;
+	//// バッファの場合はこれにする決まり
+	//vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	//// 実際に頂点リソースを生成する
+	//ID3D12Resource* vertexResource = nullptr;
+	//HRESULT hr = dxCommon->GetDevice()->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
+	//assert(SUCCEEDED(hr)); // うまくいかなかったときは起動できない
 
-	// VertexBufferViewを作成する-----------------------------------------
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
-	// リソースの先頭アドレスから使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	// 使用するリソースのサイズは頂点三つ分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(Vector4) * 3;
-	// 1つの頂点のサイズ
-	vertexBufferView.StrideInBytes = sizeof(Vector4);
+	//// VertexBufferViewを作成する-----------------------------------------
+	//D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+	//// リソースの先頭アドレスから使う
+	//vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	//// 使用するリソースのサイズは頂点三つ分のサイズ
+	//vertexBufferView.SizeInBytes = sizeof(Vector4) * 3;
+	//// 1つの頂点のサイズ
+	//vertexBufferView.StrideInBytes = sizeof(Vector4);
+
+	//VertexBuffer(VertexResource,VertexResourceView)の生成
+	VertexBuffer vb;
+	vb.Create(sizeof(Vector4) * 3, sizeof(Vector4));
 
 	// 頂点リソースにデータを書き込む-------------------------------------------
 	Vector4* vertexData = nullptr;
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
 	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
 	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
 	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
 	// 頂点リソースのマップを解除する
-	vertexResource->Unmap(0, nullptr);
+	//vb.Get()->Unmap(0, nullptr);
 
 	// メインループ
 	while (true) {
@@ -138,7 +142,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		// コマンドを積む
 		commandList->SetGraphicsRootSignature(rs.Get());          // RootSignatureの設定
 		commandList->SetPipelineState(pipelineState.Get());     // PSOの設定をする
-		commandList->IASetVertexBuffers(0, 1, &vertexBufferView); // VBVの設定をする
+		commandList->IASetVertexBuffers(0, 1, vb.GetView());          // VBVの設定をする
 		// トポロジの設定
 		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		// 頂点数、インデクス数、インデックスの開始位置、インデックスのオフセット
@@ -149,7 +153,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	}
 
 	// 解放処理
-	vertexResource->Release();
+	//vertexResource->Release();
 	//graphicsPipeLineState->Release();
 
 	// エンジンの終了処理
