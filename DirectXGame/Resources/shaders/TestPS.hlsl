884ec003781c1c84a49185072cb46a947559c41a
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-06-18 12:53:00 +0900

    白い画面にならない

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -1,15 +1,11 @@
-#include "Test.hlsli"
-
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
 };
 
-PixelShaderOutput main(VertexShaderOutput input)
+PixelShaderOutput main()
 {
     PixelShaderOutput output;
-    float32_t2 uv = input.texcoord;
-    //位置セット(x y z w )かカラーセット(r g b a)でアクセスできる
-    output.color = float32_t4(uv.x, uv.y, 0.0f, 1.0f);
+    output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
     return output;
 }
